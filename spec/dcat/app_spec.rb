f4fa18e09be69c29232efbe572ad849c3ab60d11
pikesley <github@orgraphone.org>
2016-02-05 11:17:26 +0000

    Another passing spec

--- spec/dcat/app_spec.rb
+++ spec/dcat/app_spec.rb
@@ -1,5 +1,6 @@
 describe MetricsApi do
   before :all do
+  #  WebMock.allow_net_connect!
     get '/metrics'
     body = last_response.body
     stub_request(:any, /.*/).to_return(status: 404)
@@ -9,5 +10,9 @@ describe MetricsApi do
 
   it 'has a title' do
     expect(@dataset.data_title).to eq 'ODI Metrics'
+  end
+
+  it 'has the correct URL' do
+    expect(@dataset.send(:dataset_uri)).to eq 'http://example.org/metrics'
   end
 end
