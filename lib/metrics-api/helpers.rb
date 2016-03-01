ec35ba86bd5dcc15f1ea6cbcb6d94d719c0217aa
pezholio <pezholio@gmail.com>
2016-03-01 15:13:23 +0000

    Generate image for CC licenses

--- lib/metrics-api/helpers.rb
+++ lib/metrics-api/helpers.rb
@@ -76,7 +76,8 @@ module Helpers
       description: ENV['METRICS_API_DESCRIPTION'],
       license: {
         name: ENV['METRICS_API_LICENSE_NAME'],
-        url: ENV['METRICS_API_LICENSE_URL']
+        url: ENV['METRICS_API_LICENSE_URL'],
+        image: license_image(ENV['METRICS_API_LICENSE_URL'])
       },
       publisher: {
         name: ENV['METRICS_API_PUBLISHER_NAME'],
@@ -84,6 +85,15 @@ module Helpers
       },
       certificate_url: ENV['METRICS_API_CERTIFICATE_URL']
     }
+  end
+
+  def license_image(url)
+    match = url.match /https?:\/\/creativecommons.org\/licenses\/([a-z\-]+)\/([0-9\.]+)/
+    if match
+      "https://licensebuttons.net/l/#{match[1]}/#{match[2]}/88x31.png"
+    else
+      nil
+    end
   end
 
 end
--- spec/helpers/helpers_spec.rb
+++ spec/helpers/helpers_spec.rb
@@ -51,6 +51,16 @@ describe Helpers do
       '2013 Q1 Completed Tasks'
     )
   end
+
+  it 'gets a image url for a creative commons license' do
+    image = helpers.license_image('https://creativecommons.org/licenses/cc-by/4.0/')
+    expect(image).to eq('https://licensebuttons.net/l/cc-by/4.0/88x31.png')
+  end
+
+  it 'returns nil for a non cc license' do
+    image = helpers.license_image('http://www.opendefinition.org/licenses/against-drm')
+    expect(image).to eq(nil)
+  end
 end
 
 describe String do
