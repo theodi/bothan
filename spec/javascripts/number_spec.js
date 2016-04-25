fe72b60d4950e3456e4c5f7fb580f3cd3e364d14
pezholio <pezholio@gmail.com>
2016-04-25 12:58:34 +0100

    Set up number view to use shadow DOM

--- spec/javascripts/number_spec.js
+++ spec/javascripts/number_spec.js
@@ -1,6 +1,6 @@
 describe('number.js', function() {
   beforeEach(function() {
-    fixture = setFixtures("<div id='number'></div>")
+    fixture = setFixtures("<div id='number-wrapper'><div id='number'></div></div>")
     spyOn(window, 'countUp')
   })
 
@@ -30,7 +30,7 @@ describe('number.js', function() {
     })
 
     it('applies a number and title', function() {
-      number(json, 'My Awesome Title')
+      number(json, 'My Awesome Title', $('#number-wrapper'))
       expect($('#number h1').html()).toEqual('My Awesome Title')
       expect($('#number #metric-value').html()).toEqual('224')
       expect($('#number small').html()).toEqual('Last updated: 2016-02-03 14:27')
@@ -50,7 +50,7 @@ describe('number.js', function() {
     })
 
     it('applies a number and title', function() {
-      number(json, 'My Awesome Title')
+      number(json, 'My Awesome Title', $('#number-wrapper'))
       expect($('#number h1').html()).toEqual('My Awesome Title')
       expect($('#number #metric-value').html()).toEqual('2')
       expect($('#number small').html()).toEqual('Last updated: 2015-01-05 11:21')
