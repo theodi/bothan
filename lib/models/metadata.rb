7116b14beaf69b99fc912bacdc3e95936f8ba559
pikesley <github@orgraphone.org>
2016-06-28 11:48:40 +0100

    Handle datatypes

--- lib/models/metadata.rb
+++ lib/models/metadata.rb
@@ -7,8 +7,10 @@ class MetricMetadata
   field :type,  type: String
   field :title, type: Hash, default: {}
   field :description, type: Hash, default: {}
+  field :datatype, type: String
 
   validates_inclusion_of :type, in: ['chart', 'tasklist', 'target', 'pie'], allow_nil: true
+  validates_inclusion_of :datatype, in: ['percentage', 'currency'], allow_nil: true
 
   index({ name: 1 }, { background: true })
 end
