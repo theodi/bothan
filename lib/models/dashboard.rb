feb0236d0f2ae8c77175e2c8a74d931c4ccdab6e
pezholio <pezholio@gmail.com>
2016-09-26 08:58:25 +0100

    Generate slug on create

--- lib/models/dashboard.rb
+++ lib/models/dashboard.rb
@@ -3,9 +3,17 @@ require 'mongoid'
 class Dashboard
   include Mongoid::Document
 
+  before_create :create_slug
+
   field :slug,  type: String
   field :name,  type: String
   field :rows,  type: String
   field :columns,  type: String
   field :metrics
+
+  def create_slug
+    self.slug = self.name.parameterize
+  end
+
+  index({ slug: 1 } , { unique: true })
 end
