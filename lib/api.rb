3a0bc60900eeca8afd1ae5859a16e89682ee0ac3
Stephen Fortune <stephen.fortune@theodi.org>
2017-04-06 17:03:06 +0100

    Date range endpoint included
    Refactor of grape needed - utilising string namespaces rather than
    nested route_params

--- lib/api.rb
+++ lib/api.rb
@@ -12,7 +12,7 @@ Metric.create_indexes
 
 module Bothan
   class Api < Grape::API
-
+    format :json
     helpers do
       def update_metric(name, time, value)
         @metric = Metric.new({
@@ -20,9 +20,18 @@ module Bothan
             "time" => time,
             "value" => value
         })
+
+        metadata = MetricMetadata.find_or_create_by(name: name.parameterize)
+
+        if metadata.title.blank?
+          # binding.pry
+          metadata.title[:en] = name
+          metadata.save
+        end
+
         if @metric.save
           Pusher.trigger(name.parameterize, 'updated', {})
-          return 201
+          return 201 # status instead of return???
         else
           return 500
         end
@@ -31,9 +40,10 @@ module Bothan
 
     namespace :metrics do
 
-      desc 'list all available metrics'
+      desc 'list all available metrics' # conflicts with sinatra
       # /metrics[.json]
-      get do
+      # get '.json' do # this feels like a very UGLY workaround to conflict problem - confirmed when it doesnt solve problems elsewhere
+      get do # this feels like a very UGLY workaround to conflict problem - confirmed when it doesnt solve problems elsewhere
         @metrics = {
           metrics: Metric.all.distinct(:name).sort.map do |name|
             {
@@ -53,28 +63,54 @@ module Bothan
           requires :value, type: Integer, desc: 'metric value'
         end
         post do
-          # binding.pry
-          # above will retrive metric as a parameter within the params hash along with params passed by body
           update_metric(params[:metric], params[:time], params[:value])
         end
       end # end route_param
+    end
+
+    namespace 'metrics/:metric' do # required because nested namespace or something
+
+      desc 'create a single metric'
+
+      params do
+        requires :metric, type: String, desc: 'new metric'
+        requires :time, type: String, desc: 'metric timestamp'
+        requires :value, type: Integer, desc: 'metric value'
+      end
+      post do
+        # above will retrive metric as a parameter within the params hash along with params passed by body
+        update_metric(params[:metric], params[:time], params[:value])
+      end
 
       desc 'show latest value for given metric' # /metrics/{metric_name}[.json]
-      route_param :metric do
-        params do
-          requires :metric, type: String, desc: 'metric names'
-        end
-        get do
-          @metric = Metric.where(name: params[:metric].parameterize).order_by(:time.asc).last
-          @metric.to_json
-        end
+      params do
+        requires :metric, type: String, desc: 'metric names'
+      end
+      get do
+
+        @metric = Metric.where(name: params[:metric].parameterize).order_by(:time.asc).last
+        @metric.to_json
       end
 
       desc 'show value for given metric at a given time (defaults to current time)' # /metrics/{metric_name}/{time}
 
+    end
+
+    namespace 'metrics/:metric/:start_date/:end_date' do
       desc 'list values for given metric between given range' # /metrics/{metric_name}/{from}/{to}
 
+      params do
+        requires :start_date, type: DateTime
+        requires :end_date, type: DateTime
+      end
+      get do
+        binding.pry
+        {searchstring: "will return vals from "+params[:start_date]+" until "+params[:end_date]}
+      end
+
     end
+    
+    # OLD API CODE
 
     # Api.post '/metrics/:metric' do
     #   protected!
