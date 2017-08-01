fd871861509348be00dd4a5762fba5c7b0510fc3
Stephen Fortune <stephen.fortune@theodi.org>
2017-08-01 17:24:01 +0100

    Include protected paths for testing

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -12,7 +12,11 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+    when /dashboard/
+      '/dashboards/new'
 
+    when /login/
+      '/login'
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
