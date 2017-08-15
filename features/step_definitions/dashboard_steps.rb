59ea9f92e57e12493d7343d5bcd261aea70373a8
Stephen Fortune <stephen.fortune@theodi.org>
2017-08-15 15:27:55 +0100

    step for evaluating dashboard editor UI

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -1,3 +1,10 @@
+Then /^the dashboard editor has "([^\"]*)" rows and "([^\"]*)" columns$/ do |row_count, column_count|
+  within('table#dashboard') do
+    expect(page).to have_xpath(".//tr", :count => row_count)
+    expect(page).to have_xpath(".//td", :count => column_count)
+  end
+end
+
 Then /^the dashboard "([^\"]*)" has "([^\"]*)" panels$/ do |slug, number|
   # byebug
   list = find("##{slug}").all('li')
