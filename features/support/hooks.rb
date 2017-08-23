74d8b099d7585d19262d580c55c3b2c7981bcda0
Stephen Fortune <stephen.fortune@theodi.org>
2017-08-23 12:42:08 +0100

    remove time from metric as not mandatory attribute

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,6 +8,6 @@ After do
 end
 
 Before ('@javascript') do
-  Metric.create(name: "dashboard-0", time: "2017-08-03T15:00:00+00:00")
+  Metric.create(name: "dashboard-0")
   # TODO - is there a better way to accomplish this
 end
