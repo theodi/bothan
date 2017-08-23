4eee3d5bdf69b16b4550b8e4bd2073c26a844125
Stephen Fortune <stephen.fortune@theodi.org>
2017-08-23 12:09:26 +0100

    Make use of the paths file
    Hopefully this will become more DRY the more user interaction features
    are added

--- features/step_definitions/web_driver_steps.rb
+++ features/step_definitions/web_driver_steps.rb
@@ -1,5 +1,5 @@
 Given /^(?:I|I'm) (?:login|logged in) with user: "([^\"]*)" and pwd: "([^\"]*)" and visit "([^\"]*)"$/ do |username, password, path |
-  page.visit("http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/#{path}")
+  page.visit("http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/#{path_to(path)}")
 end
 
 Then /^the current URL path is (.+)$/ do |url_path|
