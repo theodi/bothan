c69fd3e9801e5777697007723167de8efde3c0dc
Stephen Fortune <stephen.fortune@theodi.org>
2017-08-31 17:41:05 +0100

    configure POST endpoints with Grape syntax

--- lib/api.rb
+++ lib/api.rb
@@ -9,55 +9,65 @@ require 'models/dashboard'
 
 require 'bothan/extensions/date_wrangler'
 
+require 'bothan/helpers/metrics_helpers'
+
 Mongoid.load!(File.expand_path("../mongoid.yml", File.dirname(__FILE__)), ENV['RACK_ENV'])
 Metric.create_indexes
 
 module Bothan
   class Api < Grape::API
     content_type :json, 'application/json; charset=UTF-8'
-    def self.registered(app)
 
-      app.post '/metrics/:metric' do
-        protected!
-        body = JSON.parse request.body.read
+    helpers Bothan::Helpers::Metrics
 
-        update_metric(params[:metric], body["time"], body["value"])
+    namespace :metrics do
+      desc 'create a single metric'
+
+      post '/:metric' do
+        params do
+          # requires :metric, type: String, desc: 'new metric'
+          requires :time, type: String, desc: 'metric timestamp'
+          requires :value, types: [Integer, Hash], desc: 'metric value or values'
+        end
+
+        # format :json #TODO - when refactoring into classes make this explicit at top of class to reduce Headers passed via curl
+        update_metric(params[:metric], params[:time], params[:value])
       end
+    end
 
-      app.post '/metrics/:metric/increment/?:amount?' do
-        protected!
+    namespace 'metrics/:metric' do
 
+      desc 'increment a metric' # home/metrics/:metric/increment"
+      params do
+        requires :amount, type: Integer, desc: 'amount to increase by'
+        # requires :amount, coerce: Integer
+      end
+      post '/:increment' do
         last_metric = Metric.where(name: params[:metric].parameterize).last
         last_amount = last_metric.try(:[], 'value') || 0
-
-        # Return 400 if the metric type is anything other than a single metric
         return 400 if last_amount.class == BSON::Document
-
         increment = (params[:amount] || 1).to_i
         value = last_amount + increment
-
         update_metric(params[:metric], DateTime.now, value)
       end
 
-      app.post '/metrics/:metric/metadata' do
-        protected!
-        begin
-          data = JSON.parse request.body.read
-          @meta = MetricMetadata.find_or_create_by(name: params[:metric].parameterize)
-          @meta.type = data["type"].presence
-          @meta.datatype = data["datatype"].presence
-          @meta.title.merge!(data["title"] || {})
-          @meta.description.merge!(data["description"] || {})
-          if @meta.save
-            return 201
-          else
-            return 400
-          end
-        rescue
-          return 500
+    end
+
+    namespace 'metrics/:metric/metadata' do
+      post do
+        # format :json #TODO - when refactoring into classes make this explicit at top of class
+        @meta = MetricMetadata.find_or_create_by(name: params[:metric].parameterize)
+        @meta.type = params[:type].presence
+        @meta.datatype = params[:datatype].presence
+        @meta.title.merge!(params[:title] || {}) # TODO these are breaking when used with grape, because they are set as hash in mongo document, N2K if they are to be kept as such
+        @meta.description.merge!(params[:description] || {}) # TODO these are breaking when used with grape, because they are set as hash in mongo document, N2K if they are to be kept as such
+        if @meta.save # TODO - this endpoint exits here
+          status 201
+        else
+          status 400
+          # maybe superfluous given Grape automatically returns this on POST success
         end
       end
-
     end
 
   end
