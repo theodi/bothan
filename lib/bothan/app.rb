e33f5d633ddb6ef4ff342e622b4215018eaac7e1
Stephen Fortune <stephen.fortune@theodi.org>
2017-09-06 16:19:31 +0100

    Dependencies for Sinatra app

--- lib/bothan/app.rb
+++ lib/bothan/app.rb
@@ -1,14 +1,14 @@
-$:.unshift File.dirname(__FILE__)
-
-require 'action_view'
-require 'github/markup'
-
-require 'extensions/string'
-
-require 'helpers/app_helpers'
-require 'helpers/auth_helpers'
-require 'helpers/metrics_helpers'
-require 'helpers/views_helpers'
-require 'helpers/dashboard_helpers'
-require 'byebug'
-
+# $:.unshift File.dirname(__FILE__)
+#
+# require 'action_view'
+# require 'github/markup'
+#
+# require 'extensions/string'
+#
+# require 'helpers/app_helpers'
+# require 'helpers/auth_helpers'
+# require 'helpers/metrics_helpers'
+# require 'helpers/views_helpers'
+# require 'helpers/dashboard_helpers'
+# require 'byebug'
+#
