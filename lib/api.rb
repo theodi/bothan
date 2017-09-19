8a0401a25adbdfdf48b2b5856c9a271444da4e98
Stephen Fortune <stephen.fortune@theodi.org>
2017-09-19 18:05:03 +0100

    Large reorganisation of API, see commit msg
    Logic reshuffled for use with Swagger-UI.
    There is now effectively one namespace - metrics. The <metric>/metadata
    endpoint is kept for now to illustrate differences in grape-entity vs
    vanilla grape in terms of how documentation bubbles up to swagger-ui.
    The majority of methods/blocks are nested under :metrics to keep the
    :metric param description is DRY

--- lib/api.rb
+++ lib/api.rb
@@ -36,7 +36,7 @@ module Bothan
   end
 
   # class EndpointAliases < Grape::Validations::Base
-  # TODO - use this block if/when this Ruby bug is fixed https://bugs.ruby-lang.org/issues/13661
+  # # TODO - use this block if/when this Ruby bug is fixed https://bugs.ruby-lang.org/issues/13661
   #   def validate_param!(attr_name, params)
   #     unless ['all','latest', 'today', 'since-midnight','since-beginning-of-month','since-beginning-of-week', 'since-beginning-of-year'].include?(params[attr_name])
   #       fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: 'must be a recognised endpoint alias'
@@ -64,6 +64,7 @@ module Bothan
     end
 
     rescue_from Grape::Exceptions::ValidationErrors do |e|
+      # byebug
       if e.message == "timespan is invalid"
         error!({status: "time is not a valid ISO8601 date/time."}, 400)
       else
@@ -77,149 +78,145 @@ module Bothan
 
     namespace :metrics do
 
-      desc 'list all available metrics' # conflicts with sinatra
+      desc 'list all available metrics'
 
       get do
-
         metrics = list_metrics
         present metrics, with: Bothan::Entities::MetricList
-        # Bothan::Entities::MetricList.represent(metrics)
       end
 
-
-      desc 'create a single metric'
-
-      post '/:metric' do
-
-        endpoint_protected!
-        params do
-          requires :time, type: String, desc: 'metric timestamp'
-          requires :value, types: [Integer, Hash], desc: 'metric value or values'
-        end
-
-        # format :json #TODO - when refactoring into classes make this explicit at top of class to reduce Headers passed via curl
-        update_metric(params[:metric], params[:time], params[:value])
-        if @metric.save
-          present @metric, with: Bothan::Entities::Metric
-        end
-      end
-    end
-
-    namespace 'metrics/:metric' do
-
-      desc 'show latest value for given metric' # /metrics/{metric_name}[.json]
       params do
-        requires :metric, type: String, desc: 'metric names'
+        requires :metric, type: String, desc: 'The name identifier where your metrics are stored'
       end
-      get do
-        # metric = Metric.where(name: params[:metric].parameterize).order_by(:time.asc).last
-        metric = metric(params[:metric])
-        # byebug
-        present metric, with: Bothan::Entities::Metric
-      end
+      route_param :metric do
+      # params do, followed by route_param used to namespace enables the documentation desc for :metric to persist in all subsequent blocks
+      # however using this method means that the documentation from Bothan::Entities does not surface
+      # namespace 'metrics/:metric' do # can also be '/:metrics', but never :metric
 
-      desc 'delete an entire metric endpoint'
-      delete do
-        endpoint_protected!
-        params do
-          requires :metric, type: String, desc: 'metric names'
-        end
-        Metric.where(name: params[:metric]).destroy_all
-      end
-
-      desc 'time queries'
-      params do
-        optional :timespan, type: TimeSpanQueries
-      end
-      get ':timespan' do
-        if %w(all latest today since-midnight since-beginning-of-month since-beginning-of-week since-beginning-of-year).include?(params[:timespan].value)
-          range_alias(params[:timespan].value)
-        else
-          metric = metric(params[:metric], params[:timespan].value.to_datetime)
+        desc 'show latest value for given metric'
+        get do
+          metric = metric(params[:metric])
           present metric, with: Bothan::Entities::Metric
         end
-      end
 
-      # desc 'time queries'
-      # TODO - use this block instead of above block of same desc if/when this Ruby bug is fixed https://bugs.ruby-lang.org/issues/13661
-      # TODO - currently the below logic, and associated Grape::Validations::Base class EndpointAliases do not work because of 'since-beginning-month'
-      # params do
-      #   requires :timespan, types: [DateTime, EndpointAliases]
-      # end
-      # get '/:timespan' do
-      #   if params[:timespan].to_datetime.instance_of?(DateTime)
-      #       metric = single_metric(params[:metric], params[:timespan].to_datetime)
-      #       metric
-      #   else
-      #     range_alias(params[:timespan])
-      #   end
-      # end
-
-    end
-
-    namespace 'metrics/:metric/increment' do
-      desc 'increment a metric' # home/metrics/:metric/increment"
-
-      post do
-        endpoint_protected!
-        increment_metric(1)
-        status 201
-        present @metric, with: Bothan::Entities::Metric
-      end
-
-      post '/:amount' do
-
-        endpoint_protected!
+        desc 'create a single metric',
+             detail: 'create an endpoint by posting form data',
+             params: Bothan::Entities::Metric.documentation # this is preferred as it lists the parameters as form data
         params do
-          requires :amount, type: {value: Integer}, desc: 'amount to increase by'
+          requires :time, type: DateTime, desc: 'the timestamp associated with a stored value'
+          requires :value, desc: 'the value to store alongside a timestamp'
         end
-        increment = (params[:amount] || 1).to_i
-        increment_metric(increment)
-
-        if @metric.save
-          status 201
-          present @metric, with: Bothan::Entities::Metric
+        post do
+          endpoint_protected!
+          update_metric(params[:metric], params[:time], params[:value])
+          if @metric.save
+            present @metric, with: Bothan::Entities::Metric
+          end
         end
 
-      end
-    end
+        desc 'delete an entire metric endpoint'
+        delete do
+          endpoint_protected!
+          params do
+            requires :metric, type: String, desc: 'The name identifier where your metrics are stored'
+          end
+          Metric.where(name: params[:metric]).destroy_all
+        end
 
+        desc 'time based queries, return a timestamped value or values for an alias of time'
+        params do
+          optional :timespan, type: TimeSpanQueries, desc: 'Either an alias for common timespans or a Time value'
+        end
+        get ':timespan' do
+          if %w(all latest today since-midnight since-beginning-of-month since-beginning-of-week since-beginning-of-year).include?(params[:timespan].value)
+            range_alias(params[:timespan].value)
+          else
+            metric = metric(params[:metric], params[:timespan].value.to_datetime)
+            present metric, with: Bothan::Entities::Metric
+          end
+        end
 
-    desc 'ranges'
+        # desc 'time queries'
+        # # TODO - use this block instead of above block of same desc if/when this Ruby bug is fixed https://bugs.ruby-lang.org/issues/13661
+        # # TODO - currently the below logic, and associated Grape::Validations::Base class EndpointAliases do not work because of 'since-beginning-month'
+        # params do
+        #   requires :timespan, types: [DateTime, EndpointAliases], desc: 'duration to parse'
+        #   # disappointingly the above does not surface well within Grape Swagger
+        # end
+        # get '/:timespan' do
+        #   if params[:timespan].to_datetime.instance_of?(DateTime)
+        #       metric = single_metric(params[:metric], params[:timespan].to_datetime)
+        #       metric
+        #   else
+        #     range_alias(params[:timespan])
+        #   end
+        # end
 
-    params do
-      requires :from
-      optional :to
-    end
+        desc 'list a time-series range of measurements for a given metric'
 
-    get 'metrics/:metric/:from/:to' do
-      metrics = get_timeseries(params)
-      present metrics, with: Bothan::Entities::MetricCollection
-    end
+        params do
+          requires :from, type: String, desc: 'the commencement date/time for your time-series range'
+          optional :to, type: String, desc: 'the conclusion date/time for your time-series range'
+        end
 
-    namespace 'metrics/:metric/metadata' do
+        get ':from/:to' do
+          metrics = get_timeseries(params)
+          present metrics, with: Bothan::Entities::MetricCollection
+        end
 
-      params do
-          optional :metric, type: String, desc: 'metric to be meta-dated and fed to mongo'
-          optional :type, type: String, desc: 'default metric visualisation', values: ['chart', 'tasklist', 'target', 'pie', 'number']
-          optional :datatype, type: String, desc: 'type of metrics, e.g. currency or percentage', values: ['percentage', 'currency']
-          optional :title, type: Hash, desc: 'metric title'
-          optional :description, type: Hash, desc: 'metric description'
-      end
-      post do
-        endpoint_protected!
-        # format :json #TODO - when refactoring into classes make this explicit at top of class
-        meta = MetricMetadata.find_or_create_by(name: params[:metric].parameterize)
-        meta.type = params[:type].presence
-        meta.datatype = params[:datatype].presence
-        meta.title.merge!(params[:title] || {}) # TODO these are breaking when used with grape, because they are set as hash in mongo document, N2K if they are to be kept as such
-        meta.description.merge!(params[:description] || {}) # TODO these are breaking when used with grape, because they are set as hash in mongo document, N2K if they are to be kept as such
-        meta.save
-        status 201
-        present meta, with: Bothan::Entities::MetricMetadata
+        namespace '/increment' do
+          desc 'auto-increment a metric'
 
-      end
-    end
-    
+          post do
+            endpoint_protected!
+            increment_metric(1)
+            status 201
+            present @metric, with: Bothan::Entities::Metric
+          end
+
+          desc 'increment a metric by stipulated amount' # home/metrics/:metric/increment"
+          params do
+            requires :amount, type: {value: Integer}, desc: 'amount to increase by'
+          end
+          post '/:amount' do
+            endpoint_protected!
+            increment = (params[:amount] || 1).to_i
+            increment_metric(increment)
+
+            if @metric.save
+              status 201
+              present @metric, with: Bothan::Entities::Metric
+            end
+
+          end
+        end
+
+        namespace '/metadata' do
+          desc 'update a metrics metadata',
+              detail: 'update below parameters by posting form data',
+              params: Bothan::Entities::MetricMetadata.documentation
+          post do
+            endpoint_protected!
+            # format :json #TODO - when refactoring into classes make this explicit at top of class
+            meta = MetricMetadata.find_or_create_by(name: params[:metric].parameterize)
+            meta.type = params[:type].presence
+            meta.datatype = params[:datatype].presence
+            meta.title.merge!(params[:title] || {}) # TODO these are breaking when used with grape, because they are set as hash in mongo document, N2K if they are to be kept as such
+            meta.description.merge!(params[:description] || {}) # TODO these are breaking when used with grape, because they are set as hash in mongo document, N2K if they are to be kept as such
+            if meta.save
+              status 201
+              present meta, with: Bothan::Entities::MetricMetadata
+            else
+              status 400
+            end
+          end
+        end
+
+        # end # END /<metric> namespace
+      end # end route param
+
+    end # end metrics namespace
+
+    add_swagger_documentation
   end
 end
